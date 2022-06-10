1f8a0f9b5ba371cc4ebaedecbe282b1883d7047e
Lucas <Lucas@Lucas-PC>
2022-06-09 21:39:51 -0300

    Crud + Form utilizando ThymeLeaf Não implementados via form delete e update

---
+++ AcademiaJpaeRestThLeaf2/src/main/java/com/AcademiaJPARest/AcademiaJpaeRest/AcademiaJpaeRestThLeaf2Application.java
@@ -0,0 +1,13 @@
+package com.AcademiaJPARest.AcademiaJpaeRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaJpaeRestThLeaf2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaJpaeRestThLeaf2Application.class, args);
+	}
+
+}
---
+++ AcademiaJpaeRestThLeaf2/src/main/java/com/AcademiaJPARest/AcademiaJpaeRest/Controllers/ProdutoController.java
@@ -0,0 +1,71 @@
+package com.AcademiaJPARest.AcademiaJpaeRest.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import com.AcademiaJPARest.AcademiaJpaeRest.Models.Produto;
+import com.AcademiaJPARest.AcademiaJpaeRest.Repositories.ProdutoRepository;
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutoController {
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@GetMapping ("/cadastrar")
+	public String getprodutos(Model model) {
+		model.addAttribute("produto", new Produto());
+		return "cadastrarProduto";
+	}
+
+	@PostMapping("/cadastrar")
+	public String adicionar(@ModelAttribute Produto produto, Model model) {
+		produtoRepository.save(produto);
+		List<Produto> listaProdutos = produtoRepository.findAll();
+		model.addAttribute("produtos", listaProdutos);
+		return "listarProdutos";
+		
+	}
+
+	@GetMapping("/{id}")
+	Produto getproduto(@PathVariable Long id) {
+
+		return produtoRepository.findById(id).get();
+
+	}
+
+	@DeleteMapping("/excluir/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	void deleteproduto(@PathVariable Long id) {
+		produtoRepository.deleteById(id);
+	}
+
+	@PutMapping("/alterar/{id}")
+	Produto updateproduto(@RequestBody Produto novoProduto, @PathVariable Long id) {
+		Produto p = produtoRepository.findById(id).get();
+		if ((novoProduto.getCodigo() != 0)) {
+			p.setCodigo(novoProduto.getCodigo());
+		}
+		if (novoProduto.getNome() != null) {
+			p.setNome(novoProduto.getNome());
+		}
+		p.setCategoria(novoProduto.getCategoria());
+		p.setValor(novoProduto.getValor());
+		p.setQuantidade(novoProduto.getQuantidade());
+		return produtoRepository.save(p);
+
+	}
+
+}
